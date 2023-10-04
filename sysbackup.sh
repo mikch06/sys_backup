a14e1b5ff9610ed3be6c6aa96ac1c5153d6b93d6
mik <mail@kissel.ch>
2023-10-04 22:25:06 +0200

    renames and insert mount. fixes in comment and log

--- sysbackup.sh
+++ sysbackup.sh
@@ -6,32 +6,38 @@
 # Backup Script for unix hosts
 # Script performs a backup from local files to
 # a remote system (NFS), run by cronjob.
+# Prepare mountpoint before!
+
+# debug
+#set -x
+
 
 # Logfile
 LOGFILE="backup.log"
-exec 3>&1 1>>"$LOGFILE" 2>&1
+#exec 3>&1 1>>"$LOGFILE" 2>&1
 printf "\n###\nStart backup\n"
 date
-
-
 
 # Read backup config
 . config.cfg
 
+# Mount backup mountpoint
+sudo mount -t nfs $bak_mount
+
 # Check if backup folder exists, create if not
-if [[ ! -e $dst_host/$src_host ]]; then
+if [[ ! -e $dst_dir/$src_dir ]]; then
   echo "Create backup dir"
-  mkdir $dst_host/$src_host
+  mkdir $dst_dir/$src_dir
 fi
 
 # Check directory for current date
 today=$(date +%Y-%m-%d)
-if [[ ! -e $dst_host/$src_host/$today ]]; then
+if [[ ! -e $dst_dir/$src_dir/$today ]]; then
   printf "Create backup daily dir"
-  mkdir $dst_host/$src_host/$today
+  mkdir $dst_dir/$src_dir/$today
 fi
 
 for path in $backups; do
-  printf "Backup of $path to $dst_host/$src_host/$today"
-  cp -r $path $dst_host/$src_host/$today
+  printf "Backup of $path to $dst_dir/$src_dir/$today"
+  cp -r $path $dst_dir/$src_dir/$today
 done
